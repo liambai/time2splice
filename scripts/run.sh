275f9e3bccf57cfa947e1fb94064884c4c919148
Liam <liambai@liams-mbp-2.devices.brown.edu>
2022-02-26 18:02:23 -0500

    submit script

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#SBATCH -n 1
+#SBATCH --mem=16G
+#SBATCH -t 1:00:00
+
+module load anaconda/2020.02
+source /gpfs/runtime/opt/anaconda/2020.02/etc/profile.d/conda.sh
+conda activate time2splice_env
+
+DATADIR=$1
+OUTDIR=$2
+
+python3 ./preprocess/1_create_folder_structure.py OUTDIR
+./2_run_fastQC.sh 1 ${DATADIR} ${OUTDIR}/time2splice/results/preprocess/fastqc
+./3_run_trim_galore.sh 1 
