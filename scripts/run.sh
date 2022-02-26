2a5d4d36c72ccfba40c534c32cf81a180d3ec5d2
Liam <liambai@liams-mbp-2.devices.brown.edu>
2022-02-26 18:12:08 -0500

    fix outdir path

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,6 @@ conda activate time2splice_env
 DATADIR=$1
 OUTDIR=$2
 
-python3 ./preprocess/1_create_folder_structure.py OUTDIR
-./2_run_fastQC.sh 1 ${DATADIR} ${OUTDIR}/time2splice/results/preprocess/fastqc
-./3_run_trim_galore.sh 1 
+python3 ./preprocess/1_create_folder_structure.py ${OUTDIR}
+./preprocess/2_run_fastQC.sh 1 ${DATADIR} ${OUTDIR}/time2splice/results/preprocess/fastqc
+# ./3_run_trim_galore.sh 1 
