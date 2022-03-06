c4bd639e182891782c42084302c45ec0c74a3aca
Liam <liambai@liams-mbp-2.myfiosgateway.com>
2022-03-06 14:03:36 -0500

    run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -n 1
 #SBATCH --mem=16G
-#SBATCH -t 4:00:00
+#SBATCH -t 6:00:00
 
 module load anaconda/2020.02
 source /gpfs/runtime/opt/anaconda/2020.02/etc/profile.d/conda.sh
@@ -11,11 +11,11 @@ conda activate time2splice_env
 DATADIR=$1
 OUTDIR=$2
 
-# rm -r ${OUTDIR}
+rm -r ${OUTDIR}
 
 PREPROCESS_RES_DIR=${OUTDIR}/time2splice/results/preprocess
 
-# python3 ./preprocess/1_create_folder_structure.py ${OUTDIR}
-# ./preprocess/2_run_fastQC.sh 1 ${DATADIR}/fastq ${PREPROCESS_RES_DIR}/fastqc
-# ./preprocess/3_run_trim_galore.sh 1 ${DATADIR}/fastq ${PREPROCESS_RES_DIR}/trim_galore_fastqc 30
+python3 ./preprocess/1_create_folder_structure.py ${OUTDIR}
+./preprocess/2_run_fastQC.sh 1 ${DATADIR}/fastq ${PREPROCESS_RES_DIR}/fastqc
+./preprocess/3_run_trim_galore.sh 1 ${DATADIR}/fastq ${PREPROCESS_RES_DIR}/trim_galore_fastqc 30
 ./preprocess/4_run_Bowtie2.sh ${DATADIR}/fastq ${PREPROCESS_RES_DIR}/alignment 1 ${DATADIR}/reference/index/dmel_all_chromosome
