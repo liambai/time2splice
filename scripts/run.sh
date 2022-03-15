004a0b3ebfad6e799e9d41a0fdf7b17f10d6e749
Liam <liambai@liams-mbp-2.devices.brown.edu>
2022-03-15 01:46:47 -0400

    fix path

--- scripts/run.sh
+++ scripts/run.sh
@@ -20,4 +20,4 @@ RES_DIR=${OUTDIR}/time2splice/results
 # ./preprocess/3_run_trim_galore.sh 4 ${DATADIR}/fastq ${RES_DIR}/preprocess/trim_galore_fastqc 30
 # ./preprocess/4_run_Bowtie2.sh ${RES_DIR}/preprocess/trim_galore_fastqc ${RES_DIR}/preprocess/alignment 1 ${DATADIR}/reference/bowtie2_index/dmel_all_chromosome
 
-./rna/1_run_salmon.sh ${RES_DIR}/preprocess/alignment ${RES_DIR}/analysis/salmon ${DATADIR}/reference ${DATADIR}/reference/dmel-all-chromosome-r6.44.fasta 4 1 1 
+./rna/1_run_salmon.sh ${RES_DIR}/preprocess/trim_galore_fastqc ${RES_DIR}/analysis/salmon ${DATADIR}/reference ${DATADIR}/reference/dmel-all-chromosome-r6.44.fasta 4 1 1 
