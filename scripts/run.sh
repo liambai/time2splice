7230b9ea18e258b7cc564d4de56541c6513bd66a
Liam <liambai@liams-mbp-2.myfiosgateway.com>
2022-05-24 22:37:30 -0400

    path fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -22,6 +22,6 @@ RES_DIR=${OUTDIR}/time2splice/results
 # ./preprocess/4_run_Bowtie2.sh ${RES_DIR}/preprocess/trim_galore_fastqc ${RES_DIR}/preprocess/alignment 1 ${DATADIR}/reference/bowtie2_index/dmel_all_chromosome
 
 # ./rna/1_run_salmon.sh ${RES_DIR}/preprocess/trim_galore_fastqc ${RES_DIR}/analysis/salmon ${DATADIR}/reference ${DATADIR}/reference/dmel-all-chromosome-r6.44.fasta 4 1 1
-/rna/2_run_suppa.sh ${RES_DIR}/analysis/salmon ${RES_DIR}/analysis/suppa ${PATH_TO_SUPPA} 1
+./rna/2_run_suppa.sh ${RES_DIR}/analysis/salmon ${RES_DIR}/analysis/suppa ${PATH_TO_SUPPA} 1
 python3 ./rna/3_suppa_merge.py ${RES_DIR}/analysis/suppa
 # python3 ${PATH_TO_SUPPA}/suppa.py psiPerEvent -i ../../files-from-ashley/suppa_results_ncbi_trans/flybase_events -e ${RES_DIR}/analysis/suppa/iso_tpm_merged.txt -o ${RES_DIR}/analysis/suppa/earlyEmbryo_events 2> ${RES_DIR}/analysis/suppa/errors_iso_tpm_merged.txt
