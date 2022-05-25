c1c27c2014c3db5768dc163c4e587854142f6393
Liam <liambai@liams-mbp-2.myfiosgateway.com>
2022-05-24 23:30:09 -0400

    merge test

--- scripts/rna/3_suppa_merge.py
+++ scripts/rna/3_suppa_merge.py
@@ -11,11 +11,11 @@ args = parser.parse_args()
 
 dfs = []
 
-for file in  glob.glob(args.inputdir+"/*/iso_tmp.txt"):
+for file in glob.glob(args.inputdir+"/*/iso_tmp.txt"):
     df = pd.read_csv(file, sep="\t", index_col=0)
     df.index.names=['gene_id']
 
     dfs.append(df)
 
 df_merged = reduce(lambda left, right: pd.merge(left, right, on=['gene_id']), dfs)
-df_merged.to_csv(args.inputdir+"/iso_tpm_merged.txt",sep="\t")
+df_merged.to_csv(args.inputdir+"/iso_tpm_merged.txt", sep="\t")
