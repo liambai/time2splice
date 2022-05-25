1fe4cfac6310664a3cbd26cead94eca92552409e
Liam <liambai@liams-mbp-2.myfiosgateway.com>
2022-05-24 23:49:24 -0400

    merge

--- scripts/rna/3_suppa_merge.py
+++ scripts/rna/3_suppa_merge.py
@@ -2,20 +2,25 @@ import pandas as pd
 import argparse
 import glob
 
+import os
+
 from functools import reduce
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument("inputdir")
+parser.add_argument("suppadir")
 args = parser.parse_args()
 
-dfs = []
+files = [file for file in glob.glob(args.inputdir+"/*/iso_tmp.txt")]
+files_str = " ".join(files)
+# print(f"python3 {args.suppadir}/suppa.py joinFiles -f tpm -i {files_str}")
+os.system(f"python3 {args.suppadir}/suppa.py joinFiles -f tpm -i {files_str} -o {args.inputdir}/merged")
 
-for file in glob.glob(args.inputdir+"/*/iso_tmp.txt"):
-    df = pd.read_csv(file, sep="\t", index_col=0)
-    df.index.names=['gene_id']
+    # df = pd.read_csv(file, sep="\t", index_col=0)
+    # df.index.names=['gene_id']
 
-    dfs.append(df)
+    # dfs.append(df)
 
-df_merged = reduce(lambda left, right: pd.merge(left, right, on=['gene_id']), dfs)
-df_merged.to_csv(args.inputdir+"/iso_tpm_merged.txt", sep="\t")
+# df_merged = reduce(lambda left, right: pd.merge(left, right, on=['gene_id']), dfs)
+# df_merged.to_csv(args.inputdir+"/iso_tpm_merged.txt", sep="\t")
